58e971c5f07ad2b7053b562d4c2fc07a6c5698fe
賴逸峻 <x94u4rmp4@gmail.com>
2021-06-03 21:15:59 +0800

    model

---
+++ Web410717386/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web410717386.ViewModels;
+
+namespace Web410717386.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View(new SignUpData());
+        }
+        [HttpPost]
+        public ActionResult SignUP(SignUpData data)
+        {
+            if (data.Name.IsNullOrWhiteSpace())
+            {
+                data.NameMessage = "請輸入姓名";
+            }
+            if (data.Account.IsNullOrWhiteSpace())
+            {
+                data.AccountMessage = "請輸入帳號";
+            }
+            if (data.Password.IsNullOrWhiteSpace())
+            {
+                data.PasswordMessage = "請輸入密碼";
+            }
+            if (!data.Name.IsNullOrWhiteSpace() && !data.Account.IsNullOrWhiteSpace() && !data.Password.IsNullOrWhiteSpace())
+            {
+                data.Message = "註冊成功";
+            }
+            return View(data);
+        }
+    }
+}
+
+       
