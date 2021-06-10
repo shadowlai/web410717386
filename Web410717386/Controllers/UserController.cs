dfc7cc41226f391018469ef0bfcf8c9fd58dbefb
賴逸峻 <x94u4rmp4@gmail.com>
2021-06-10 22:08:18 +0800

    資料庫

--- Web410717386/Controllers/UserController.cs
+++ Web410717386/Controllers/UserController.cs
@@ -16,21 +16,10 @@ namespace Web410717386.Controllers
             return View(new SignUpData());
         }
         [HttpPost]
-        public ActionResult SignUP(SignUpData data)
+        public ActionResult SignUp(SignUpData data)
         {
-            if (data.Name.IsNullOrWhiteSpace())
-            {
-                data.NameMessage = "請輸入姓名";
-            }
-            if (data.Account.IsNullOrWhiteSpace())
-            {
-                data.AccountMessage = "請輸入帳號";
-            }
-            if (data.Password.IsNullOrWhiteSpace())
-            {
-                data.PasswordMessage = "請輸入密碼";
-            }
-            if (!data.Name.IsNullOrWhiteSpace() && !data.Account.IsNullOrWhiteSpace() && !data.Password.IsNullOrWhiteSpace())
+
+            if (ModelState.IsValid)
             {
                 data.Message = "註冊成功";
             }
